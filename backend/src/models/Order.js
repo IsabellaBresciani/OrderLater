813e731965ec69852240b5da3ef7cd1d48d152f1
Franco Petosa Ayala <franco.petosa15@gmail.com>
2025-09-20 21:45:58 -0300

    add state to order modekl

--- backend/src/models/Order.js
+++ backend/src/models/Order.js
@@ -22,7 +22,7 @@ const orderSchema = new mongoose.Schema({
             "rejected", 
             "cancelled"
         ], 
-        default: 'pending'
+        default: 'waiting to approve'
     }
 }, {
     timestamps: true
