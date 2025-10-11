733d57b32456e07fd36a62fc3d317fb786aad6cf
Franquinios <francopietrantuono999@gmail.com>
2025-10-11 14:46:01 -0300

    feat(orderRoutes): add GET method `/:id` endpoint to get an order by id

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const OrderController = require('../controllers/orderController');
 const asyncHandler = require('../handlers/asyncHandler');
+const authMiddleware = require('../middleware/authMiddleware');
 
 //--------------------------------  ENDPOINTS  --------------------------------
 
+router.get('/:id', authMiddleware, asyncHandler(OrderController.getOrderById));
 router.post('/', asyncHandler(OrderController.createOrder));
 
 module.exports = router;
