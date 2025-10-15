f2dc91f9960a88694f164a17cae83737e872ee78
Franquinios <francopietrantuono999@gmail.com>
2025-10-15 19:11:21 -0300

    feat(orderRoutes): add `/users/:id` endpoint

--- backend/src/routes/orderRoutes.js
+++ backend/src/routes/orderRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const OrderController = require('../controllers/orderController');
 const asyncHandler = require('../handlers/asyncHandler');
+const authMiddleware = require('../middleware/authMiddleware');
 
 //--------------------------------  ENDPOINTS  --------------------------------
 
+router.get('/users/:id', authMiddleware, asyncHandler(OrderController.getOrdersByUserId));
 router.post('/', asyncHandler(OrderController.createOrder));
 
 module.exports = router;
