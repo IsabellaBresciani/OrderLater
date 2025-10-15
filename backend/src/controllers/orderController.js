9257b804f3f672ab81f098a73f4060080b234f75
Franquinios <francopietrantuono999@gmail.com>
2025-10-15 19:10:44 -0300

    feat(orderController): add `getOrdersByUserId`

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,11 +1,43 @@
 const orderService = require('../services/orderService'); 
+const Actions = require('../constants/Actions.js')
 
 class OrderController {
 
     constructor(orderService) {
         this.orderService = orderService;
     }
+    
+    getOrdersByUserId = async (request, response) => {
+        const userId = request.params.id;
+
+        const orders = await this.orderService.getOrdersByUserId(userId);
         
+
+        orthersToSend = orders.map(order => ({
+            id: order._id,
+            total: order.total,
+            total_discount: order.total_discount,
+            shop_name: order.shop.name,
+            state: order.state,
+            actions:
+                order.state === 'waiting to approve' ? 
+                    [ Actions.CANCEL ]
+                : order.state === 'waiting for payment' ?
+                    [ Actions.PAY, Actions.CANCEL ]
+                :     
+                    [],
+            delivery_date: order.deliver_date
+        }));
+
+        return response
+            .status(200)
+            .json({ 
+                status: 'Success',
+                message: 'Orders retrieved successfully',
+                data: { orthersToSend }
+            });
+    }
+
     createOrder = async (request, response) => {
         
         const dto = request.body;
