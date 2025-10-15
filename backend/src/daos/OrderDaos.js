78196706cbe0c0099fcb793bd0e40ae9128847fe
Franquinios <francopietrantuono999@gmail.com>
2025-10-15 19:08:52 -0300

    add getOrdersByUserId method to OrderDAO for fetching user-specific orders

--- backend/src/daos/OrderDaos.js
+++ backend/src/daos/OrderDaos.js
@@ -2,6 +2,10 @@ const  Order = require('../models/Order.js');
 
 class OrderDAO {
 
+    getOrdersByUserId(userId) {
+        return Order.find({ user: userId });
+    }
+
     createOrder(order) {
         return Order.create(order);
     }
