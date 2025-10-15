06d1256ca943cd63532ceb57ebbe3eccf99da2e3
Franquinios <francopietrantuono999@gmail.com>
2025-10-15 19:54:05 -0300

    feat(orderDAO): add `getOrderById` and `updateOrder` methods

--- backend/src/daos/OrderDaos.js
+++ backend/src/daos/OrderDaos.js
@@ -1,9 +1,16 @@
 const  Order = require('../models/Order.js');
 
 class OrderDAO {
+    getOrderById(id) {
+        return Product.findById(id);
+    }
 
     getOrdersByUserId(userId) {
         return Order.find({ user: userId });
+    }
+
+    updateOrder(id, updateData) {
+        return Product.findByIdAndUpdate(id, updateData, { new: true });
     }
 
     createOrder(order) {
